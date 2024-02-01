5c51eb5db51fec1141401e20b7c8e87c982a5f30
Sharime Cahuas <sharimecahuas@gmail.com>
2024-02-01 17:25:35 -0300

    add primereact configurations

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider>
-      <App />
+      <PrimeReactProvider>
+        <App />
+      </PrimeReactProvider>
     </RouterProvider>
   </React.StrictMode>,
 )
